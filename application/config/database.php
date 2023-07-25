714f9c7d9e8677dfdaa438d5a82581f12382de28
Eric Faustino Alvim <73504394+EricFaustino@users.noreply.github.com>
2023-07-25 13:39:42 -0300

    Add files via upload

---
+++ application/config/database.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+$active_group = 'default';
+$query_builder = true;
+$db['default'] = [
+    'dsn'   => '',
+    'hostname' => 'whaticket_1.mysql.dbaas.com.br',
+    'username' => 'whaticket_1',
+    'password' => 'N85@ngh11',
+    'database' => 'whaticket_1',
+    'dbdriver' => 'mysqli',
+    'dbprefix' => '',
+    'pconnect' => false,
+    'db_debug' => (ENVIRONMENT !== 'production'),
+    'cache_on' => false,
+    'cachedir' => '',
+    'char_set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt' => false,
+    'compress' => false,
+    'stricton' => false,
+    'failover' => [],
+    'save_queries' => true
+];
